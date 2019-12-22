92acda06f68864c1d8390883a00010a198d58819
石虎虎 <huhu.shi@qq.com>
2019-12-22 18:42:00 +0800

    Initial commit

---
+++ src/test/java/com/huhu/diveinspringsecurity/DiveInSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huhu.diveinspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveInSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
